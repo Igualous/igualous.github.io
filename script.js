9b95abea1cf16d1aabb35173c2848b95796f0ee7
Igor Rodrigues <120427686+Igualous@users.noreply.github.com>
2024-09-09 12:19:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const no = document.querySelector('.no');
+const yes = document.querySelector(".yes");
+const modal = document.querySelector('.modal');
 
-no.addEventListener('mouseover', function() {
-  const random = Math.floor(Math.random() * 100);
+// EVENTS
+no.addEventListener('mouseover', function(e) {
+    e.preventDefault();
+  const random = Math.floor(Math.random() * 80);
   no.style = `
     position: fixed;
     top: ${random}%;
     left: ${random}%;
   `
 }) 
+no.addEventListener('click', function() {
+    const random = Math.floor(Math.random() * 80);
+    no.style = `
+      position: fixed;
+      top: ${random}%;
+      left: ${random}%;
+    `
+  }) 
+
+yes.addEventListener('click', function(e) {
+    e.preventDefault();
+    modal.style.display = 'flex';
+})
